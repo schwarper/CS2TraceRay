1b0294a2597502cab1b9fddf139b0e09a20c9f4b
schwarper <75811921+schwarper@users.noreply.github.com>
2025-04-12 22:14:11 +0300

    feat: Add extensions

--- CS2TraceRay/Class/Address.cs
+++ CS2TraceRay/Class/Address.cs
@@ -4,7 +4,7 @@ namespace CS2TraceRay.Class;
 
 internal static class Address
 {
-    public static unsafe nint GetAbsoluteAddress(nint addr, nint offset, int size)
+    public static unsafe IntPtr GetAbsoluteAddress(IntPtr addr, IntPtr offset, int size)
     {
         if (addr == IntPtr.Zero)
             throw new Exception("Failed to find RayTrace signature.");
